076642b0b9d5d710792765dd21782000b0082c91
Lonar03 <152428941+Lonar03@users.noreply.github.com>
2025-12-09 14:31:26 +0100

    enemy navigation works yippee
    created an enemy that follows the player around the map. created a testscene to play around with some settings and future obstacles. also removed unnecessary files

--- Characters/Enemy/agent.gd
+++ Characters/Enemy/agent.gd
@@ -4,33 +4,41 @@ extends CharacterBody2D
 @export var speed : float = 200
 @export var health : float = 50
 var in_range : bool = false
+var player : Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("Nav map:", NavigationServer2D.get_maps().size())
 	nav_agent.navigation_finished.connect(_on_nav_finished)
-	nav_agent.velocity_computed.connect(_on_velocity_compute)
+	
+	await NavigationServer2D.map_changed
 	
 	var players = get_tree().get_nodes_in_group("Player")
 	if players.size() > 0:
-		var player : Node2D = players[0]
+		player = players[0]
 		make_path(player.global_position)
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if not in_range:
-		var next_path_pos = nav_agent.get_next_path_position()
-		var direction = global_position.direction_to(next_path_pos)
-		var new_velocity = direction * speed * delta
-		nav_agent.velocity = new_velocity
-	else:
-		pass # hit logic
+func _physics_process(delta: float) -> void:
+	if not player:
+		print("no player")
+		return
 	
+	print("Reachable:", nav_agent.is_target_reachable())
+	print("Distance:", nav_agent.distance_to_target())
+	
+	nav_agent.target_position = player.global_position
+
+	if nav_agent.is_target_reachable():
+		var next_pos = nav_agent.get_next_path_position()
+		var dir = (next_pos - global_position).normalized()
+		velocity = dir * speed
+		move_and_slide()
+
 func _on_nav_finished():
 	in_range = true
-	
-func _on_velocity_compute(safe_velocity):
-	velocity = velocity.move_toward(safe_velocity, 100)
-	move_and_slide()
-	
+
 func make_path(pos: Vector2):
 	nav_agent.target_position = pos
 
