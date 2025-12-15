68e76d625cacaf249803908c04e95a236d056bdb
Lonar03 <152428941+Lonar03@users.noreply.github.com>
2025-12-15 15:47:46 +0100

    Add poison and frozen status effects to Enemy
    Introduces poison and frozen mechanics to the Enemy class, including variables and logic for applying, stacking, and resolving these effects. Poison inflicts periodic damage based on health and stack count, while frozen temporarily disables movement. The apply_effect function allows external application of these effects. also added the possibility to add more effects later

--- Characters/Enemy/scripts/Enemy.gd
+++ Characters/Enemy/scripts/Enemy.gd
@@ -8,12 +8,19 @@ class_name base_enemy
 @export var stop_distance : float = 120 # the distance (in pixels) of how far from the player the enemy should stop
 @export var attack_speed : float = 2 # seconds per attack
 @export var damage : float = 30
+@export var poison_damage_multiplier : float = 0.015
 const NORMAL_SCALE_X := 0.2  # used for flipping. idk why we do it this way
 var repath_cooldown : float = 0.0
 var in_range : bool = false
 var player : Node2D
 var isFlipped : bool = false
 var attack_cooldown : float = 0
+var poison_active : bool = false
+var poison_stacks : float = 0
+var poison_cooldown : float = 1
+var is_frozen : bool = false
+var frozen_cooldown : float = 2
+var frozen_stacks : float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -36,10 +43,23 @@ func _physics_process(delta: float) -> void:
 	
 	attack_cooldown -= delta
 	
+	frozen_cooldown -= delta
+	if frozen_cooldown == 0:
+		is_frozen = false
+	
+	if poison_active:
+		poison_cooldown -= delta
+		if poison_cooldown <= 0:
+			poison_cooldown = 1
+			take_damage(health * poison_damage_multiplier * poison_stacks)
+	
 	repath_cooldown -= delta
 	if repath_cooldown <= 0:
 		nav_agent.target_position = player.global_position
 		repath_cooldown = 0.2
+	
+	if is_frozen:
+		return
 	
 	if nav_agent.is_target_reachable():
 		var next_pos = nav_agent.get_next_path_position()
@@ -70,6 +90,21 @@ func take_damage(amount: float):
 	health -= amount
 	if health <= 0:
 		queue_free()
+
+func apply_effect(effect: float):
+	match effect:
+		1:
+			poison_active = true
+			if not poison_stacks >= 10:
+				poison_stacks += 1
+			else:
+				poison_stacks = 10
+		2:
+			is_frozen = true
+			frozen_stacks += 1
+			if frozen_stacks <= 5:
+				frozen_cooldown = 2
+		
 
 func attack(target: CharacterBody2D):
 	push_warning("no attack func override")
