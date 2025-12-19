a157809aadd1fcc2d4e53e156bb1e8aa02d2dd28
Lonar03 <152428941+Lonar03@users.noreply.github.com>
2025-12-19 10:40:17 +0100

    added future logic for animations for melee enemy
    added commented out logic to start the animation and wait for it to end for the attack in melee enemy.

--- Characters/Enemy/scripts/meleeEnemy.gd
+++ Characters/Enemy/scripts/meleeEnemy.gd
@@ -3,6 +3,8 @@ extends base_enemy
 func attack(target: CharacterBody2D):
 	if attack_cooldown <= 0:
 		attack_cooldown = attack_speed
+		#anim_player.play("attack")
+		#await anim_player.animation_finished
 		if target.has_method("take_damage"):
 			target.take_damage(damage)
 		else:
