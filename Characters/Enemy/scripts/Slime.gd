8c4115d33c94659daa7548ba1a2a88f79bbc836f
Lonar03 <152428941+Lonar03@users.noreply.github.com>
2026-01-06 10:02:18 +0100

    first attempt at active ability
    made a first version of the miniboss active ability. it should spawn 3 slimes.
    sadly they dont move yet and all spawn at 0, 0 which is basically useless.

--- Characters/Enemy/scripts/Slime.gd
+++ Characters/Enemy/scripts/Slime.gd
@@ -1,16 +1,33 @@
 extends base_enemy
 
-@onready var timer : Timer = Timer.new() 
+@onready var timer : Timer = Timer.new()
+@export var mini_slime : PackedScene
+var mini_slimes_to_spawn : float = 3
 
 func _ready() -> void:
 	super._ready()
 	timer.one_shot = false
+	add_child(timer)
 	if not is_instance_of(stats, MinibossResource):
 		push_warning("Wrong resource inserted for miniboss")
 		return
 	timer.wait_time = stats.ability_cooldown
 	timer.autostart = true
 	timer.timeout.connect(use_ability)
+	if timer: print("ability should work")
+	timer.start()
 
 func use_ability():
-	pass
+	var slime := mini_slime.instantiate()
+	slime.scale = Vector2(0.2, 0.2)
+	for i in range(mini_slimes_to_spawn):
+		get_parent().add_child(slime)
+		#var pos : Vector2
+		#match i:
+			#1.0:
+				#pos = position + Vector2(0, 4.5)
+			#2.0:
+				#pos = position + Vector2(3.0, 3.0)
+			#3.0:
+				#pos + position + Vector2(3.0, -3.0)
+		#slime.position = pos
