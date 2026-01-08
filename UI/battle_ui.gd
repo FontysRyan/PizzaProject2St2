2efadb60fc6ba5a23c5cc1d710df338881b061d9
TheNiceLynX <147400278+TheNiceLynX@users.noreply.github.com>
2026-01-08 11:22:41 +0100

    Add pause menu UI to battle screen
    Introduces a new pause menu to the battle UI with 'Continue' and 'Exit' buttons. Also adds a 'has_ball' variable to GameController and refactors ball texture assignment in battle_ui.gd for clarity.

--- UI/battle_ui.gd
+++ UI/battle_ui.gd
@@ -5,7 +5,8 @@ extends Control
 func _ready() -> void:
 	var stick = GameController._get_stick(GameController.equipped_stick_index).texture
 	$HBoxContainer/VBoxContainer/HotBar/StickPanel/TextureRect.texture = stick
-	$HBoxContainer/VBoxContainer/HotBar/BallPanel/TextureRect.texture = GameController._get_ball(GameController.equipped_ball_index).texture
+	var ball = GameController._get_ball(GameController.equipped_ball_index).texture
+	$HBoxContainer/VBoxContainer/HotBar/BallPanel/TextureRect.texture = ball
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
