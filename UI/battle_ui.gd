3b68d76ed6bf640421a19753e8c4b53540248ab8
TheNiceLynX <147400278+TheNiceLynX@users.noreply.github.com>
2026-01-08 12:14:27 +0100

    Replace ball TextureRect with AnimatedSprite2D in UI
    Updated BallPanel to use AnimatedSprite2D instead of TextureRect for ball display, allowing for animated ball sprites. Adjusted related code in battle_ui.gd and ball_panel.gd to set sprite_frames instead of texture.

--- UI/battle_ui.gd
+++ UI/battle_ui.gd
@@ -5,8 +5,8 @@ extends Control
 func _ready() -> void:
 	var stick = GameController._get_stick(GameController.equipped_stick_index).texture
 	$HBoxContainer/VBoxContainer/HotBar/StickPanel/TextureRect.texture = stick
-	var ball = GameController._get_ball(GameController.equipped_ball_index).texture
-	$HBoxContainer/VBoxContainer/HotBar/BallPanel/TextureRect.texture = ball
+	var ball = GameController._get_ball(GameController.equipped_ball_index).sprite_frames
+	$HBoxContainer/VBoxContainer/HotBar/BallPanel/AnimatedSprite2D.sprite_frames = ball
 	$HBoxContainer/VBoxContainer/HotBar/StickPanel.self_modulate = Color(0.500, 0.500, 0.500, 1.0)
 	$HBoxContainer/VBoxContainer/HotBar/BallPanel.self_modulate = Color(0.500, 0.500, 0.500, 1.0)
 	$HBoxContainer/VBoxContainer/HotBar/AbilityPanel.self_modulate = Color(0.500, 0.500, 0.500, 1.0)
--- UI/panels/ball_panel.gd
+++ UI/panels/ball_panel.gd
@@ -1,9 +1,9 @@
 extends Panel
-var texture : Texture2D = GameController._get_ball(GameController.equipped_ball_index).texture
+var sprites : SpriteFrames = GameController._get_ball(GameController.equipped_ball_index).sprite_frames
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$TextureRect.texture = texture
+	$AnimatedSprite2D.sprite_frames = sprites
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
