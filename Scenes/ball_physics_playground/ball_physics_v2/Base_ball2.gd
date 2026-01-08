859590b7ca823ae653afa9596a6e66954086b42c
galactorsus( <504972@vistacollege.nl>
2026-01-08 11:16:19 +0100

    Add Base_ball2 to Golf_Balls group in _ready
    The Base_ball2 node is now added to the 'Golf_Balls' group during initialization. This allows for easier group-based management and interaction with all golf ball instances.

--- Scenes/ball_physics_playground/ball_physics_v2/Base_ball2.gd
+++ Scenes/ball_physics_playground/ball_physics_v2/Base_ball2.gd
@@ -15,6 +15,7 @@ var can_be_picked_up: bool = true
 @onready var enable_mask_timer := get_tree().create_timer(2.0)
 
 func _ready():
+	add_to_group("Golf_Balls")
 	contact_monitor = true
 	max_contacts_reported = 1
 	connect("body_entered", Callable(self, "_on_body_entered"))
