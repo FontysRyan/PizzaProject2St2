e7c6c6921c9a9352740deb39ed8e65208e613427
Danté W <dantew2004@gmail.com>
2026-01-09 11:03:55 +0100

    Adjust camera zoom and improve player door transition
    Increased the camera zoom in Map.tscn for better visibility. Updated Door_Object.gd to refine player positioning when moving through doors, placing the player next to the door using a buffer. Room_Object.gd now names room instances based on their grid position for easier identification.

--- Floor Generator/Rooms/Scripts/Door_Object.gd
+++ Floor Generator/Rooms/Scripts/Door_Object.gd
@@ -16,7 +16,7 @@ var animation_translation: Dictionary[Vector2, String] = {
 	Vector2.RIGHT: "Right",
 }
 
-func _init(_orientation: Vector2, _position: Vector2, grid_position: Vector2, _room_spacing: Vector2 = Vector2.ZERO) -> void:
+func _init(_orientation: Vector2, _position: Vector2, _grid_position: Vector2, _room_spacing: Vector2 = Vector2.ZERO) -> void:
 	var door_object: Node2D = door.instantiate()
 	door_object.name = "Door"
 	add_child(door_object)
@@ -24,6 +24,7 @@ func _init(_orientation: Vector2, _position: Vector2, grid_position: Vector2, _r
 	position = _position
 	room_spacing = _room_spacing
 	orientation = _orientation
+	room_grid_position = _grid_position
 	var animation: AnimatedSprite2D = door_object.get_child(0)
 	if animation_translation.has(_orientation):
 		animation.animation = animation_translation[_orientation]
@@ -33,16 +34,12 @@ func _init(_orientation: Vector2, _position: Vector2, grid_position: Vector2, _r
 func _ready() -> void:
 	get_child(0).player_door_interact.connect(move_player_to_room)		
 		
-func move_player_to_room(door: DoorObject, player):
+func move_player_to_room(_door: DoorObject, player):
 	# This is bad practice
 	# But I frankly don't care
-	
-	# AAAAAAAAAAA IT BROKEN AND I DO NOT UNDERSTAND WHY
-	# I think it has to do with a bug in the room placement code?
-	# Will see later
-	print("Delta grid: " + str(orientation))
-	print("New grid: " + str(room_grid_position + orientation))
-	player.position = (get_viewport_rect().size + room_spacing) * (room_grid_position + orientation) + get_viewport_rect().size/2
-	print("New position: " + str(player.position))
+	# Now to make the player spawn next to the door instead
+	# Fuck it bad practice 2: electric boogaloo
+	var player_buffer: Vector2 = Vector2(50, 50)
+	player.position = self.global_position + ((room_spacing + player_buffer) * orientation)
 	get_tree().get_nodes_in_group("Camera")[0].position = (get_viewport_rect().size + room_spacing) * (room_grid_position + orientation) + get_viewport_rect().size/2
 	
