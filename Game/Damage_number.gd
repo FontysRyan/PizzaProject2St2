21388408308ee7db2e9dd700bf0938ccf0e271ed
galactorsus( <504972@vistacollege.nl>
2026-01-13 10:27:38 +0100

    damage number patch
    changed duration
    changed floating distance
    adjusted spawn location so it is above enemies and not right in front of it's sprite

--- Game/Damage_number.gd
+++ Game/Damage_number.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@export var float_distance := 5.0
-@export var duration: float = 1.0
+@export var float_distance := 10.0
+@export var duration: float = 5.0
 
 @onready var label: Label = $Label
 
