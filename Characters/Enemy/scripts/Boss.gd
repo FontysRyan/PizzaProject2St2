5a1d27051b83f932e362ebd12dc302047fd68b2e
Lonar03 <152428941+Lonar03@users.noreply.github.com>
2026-01-15 09:49:55 +0100

    Add damage and death handling to Boss
    Introduces take_damage and on_death methods to the Boss script. When the boss' health reaches zero it spawns a chest.

--- Characters/Enemy/scripts/Boss.gd
+++ Characters/Enemy/scripts/Boss.gd
@@ -14,3 +14,12 @@ func _ready() -> void:
 
 func use_ability():
 	print("no ability implemented")
+
+func take_damage(amount: float, _damage_source: Base_Ball = null):
+	health -= amount
+	if health <= 0:
+		queue_free()
+		on_death()
+
+func on_death():
+	Communication.spawn_chest(position, Chest.ChestType.STICK)
