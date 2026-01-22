2a02721c7d918ea5eb9ef869518895e290c32ae7
Ryan <547558@student.fontys.nl>
2026-01-22 15:48:29 +0100

    cutscene skip button

--- Movies/intro_cutscene.gd
+++ Movies/intro_cutscene.gd
@@ -1,39 +1,69 @@
 extends Node2D
 
-
 @export var blackOverlay: CanvasItem
-# @export var spotlightLayer: CanvasItem
-# @export var spotlightMask: CanvasItem
 @export var animationPlayer: AnimationPlayer
 @export var dungeonFloor: Node2D
 @export var tilemapLayer: TileMapLayer
 @export var playerModel: Node2D
 @export var spawnRoom: PackedScene
+@export var skipButton: Button
+
+var skipped := false
 
 
 func _ready() -> void:
-	# Safety checks (optional but recommended)
-	assert(animationPlayer, "AnimationPlayer not assigned")
-	assert(tilemapLayer, "TileMapLayer not assigned")
-	assert(spawnRoom, "SpawnRoom scene not assigned")
+	assert(animationPlayer)
+	assert(tilemapLayer)
+	assert(spawnRoom)
 
 	# Initial state
-	tilemapLayer.visible = false
-
-	# Play intro animation
-	animationPlayer.play("Intro")
-
-	# Wait 1.5 seconds, then show tilemap
-	await get_tree().create_timer(1.5).timeout
-	dungeonFloor.visible = true
-	tilemapLayer.visible = true
-
-	# Wait until animation fully finishes
-	await animationPlayer.animation_finished
-
-	# Hide tilemap again
 	dungeonFloor.visible = false
 	tilemapLayer.visible = false
 
-	# Switch scene
+	# Connect skip button
+	if skipButton:
+		skipButton.pressed.connect(_on_skip_pressed)
+
+	# Play intro
+	animationPlayer.play("Intro")
+
+	# Show dungeon after delay
+	await get_tree().create_timer(1.5).timeout
+	if skipped:
+		return
+
+	dungeonFloor.visible = true
+	tilemapLayer.visible = true
+
+	# Wait for animation to finish
+	await animationPlayer.animation_finished
+	if skipped:
+		return
+
+	_finish_intro()
+
+
+func _on_skip_pressed() -> void:
+	if skipped:
+		return
+
+	skipped = true
+
+	# Stop animation immediately
+	if animationPlayer.is_playing():
+		animationPlayer.stop()
+
+	_finish_intro()
+
+
+func _finish_intro() -> void:
+	# Prevent double execution
+	if skipped == false:
+		skipped = true
+
+	# Clean state (optional but neat)
+	dungeonFloor.visible = false
+	tilemapLayer.visible = false
+
+	# Transition
 	get_tree().change_scene_to_packed(spawnRoom)
